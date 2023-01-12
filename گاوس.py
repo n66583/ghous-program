fa16d8dbec055dec0233801e7b6300d98802d8b2
Hossain sadeqi <119123595+n66583@users.noreply.github.com>
2023-01-12 19:15:04 +0330

    Add files via upload

---
+++ گاوس.py
@@ -0,0 +1,7 @@
+start = float(input("Enter start number: "))
+second = float(input("Enter second number: "))
+end = float(input("Enter end number: "))
+step = second-start
+count = ((end-start)/step)+1
+ghous = ((end+start)/2)*count
+print("count is:", ghous)
